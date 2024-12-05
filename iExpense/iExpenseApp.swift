4c7897d679806515bf6293679dad921cdccd7d52
Lochoa11 <linochoa11690@gmail.com>
2024-12-05 15:32:51 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lin Ochoa on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
